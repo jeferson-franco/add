705b45aa45e656879a03277301b30a23e730d555
jefersonfranco <jefersonfranco@protonmail.com>
2022-08-17 14:06:46 -0300

    test: update add.test.js

--- src/add.test.js
+++ src/add.test.js
@@ -1,5 +1,25 @@
 const solution = require('./add.js');
 
-test('adds 1 + 2 to equal 3', () => {
+test('test 1', () => {
     expect(solution(1, 2)).toBe(3);
+});
+
+test('test 2', () => {
+    expect(solution(0, 1000)).toBe(1000);
+});
+
+test('test 3', () => {
+    expect(solution(2, -39)).toBe(-37);
+});
+
+test('test 4', () => {
+    expect(solution(99, 100)).toBe(199);
+});
+
+test('test 5', () => {
+    expect(solution(-100, 100)).toBe(0);
+});
+
+test('test 6', () => {
+    expect(solution(-1000, -1000)).toBe(-2000);
 });
